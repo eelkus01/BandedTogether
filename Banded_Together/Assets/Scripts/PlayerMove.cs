184ffbb43d4498c7161862a097dee9caddd224fb
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2023-10-27 13:18:57 -0400

    fixed walk to be top down (accidentally did platformer before)

--- Banded_Together/Assets/Scripts/PlayerMove.cs
+++ Banded_Together/Assets/Scripts/PlayerMove.cs
@@ -2,51 +2,44 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerMoveAround : MonoBehaviour {
 
-    //public Animator animator;
+    //public Animator anim;
+    //public AudioSource WalkSFX;
     public Rigidbody2D rb2D;
-    private bool FaceRight = true; // determine which way player is facing. 
+    private bool FaceRight = true; // determine which way player is facing.
     public static float runSpeed = 10f;
     public float startSpeed = 10f;
     public bool isAlive = true;
-    //public AudioSource WalkSFX;
-    private Vector3 hMove;
 
-    void Start() {
-        //animator = gameObject.GetComponentInChildren<Animator>();
+    void Start(){
+        //anim = gameObject.GetComponentInChildren<Animator>();
         rb2D = transform.GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
-        //NOTE: Horizontal axis: [a] / left arrow is -1, [d] / right arrow is 1 
-        hMove = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        if (isAlive == true) {
-            transform.position = transform.position + hMove * runSpeed * Time.deltaTime;
-            if (Input.GetAxis("Horizontal") != 0) {
-                //       animator.SetBool("Walk", true); 
-                //       if (!WalkSFX.isPlaying){ 
-                //             WalkSFX.Play(); 
-                //      } 
+    void Update(){
+        //NOTE: Horizontal axis: [a] / left arrow is -1, [d] / right arrow is 1
+        //NOTE: Vertical axis: [w] / up arrow, [s] / down arrow
+        Vector3 hvMove = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        if (isAlive == true){
+            transform.position = transform.position + hvMove * runSpeed * Time.deltaTime;
+
+            if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0)){
+            //     anim.SetBool ("Walk", true);
+            //     if (!WalkSFX.isPlaying){
+            //           WalkSFX.Play();
+            //     }
             } else {
-                //      animator.SetBool("Walk", false); 
-                //      WalkSFX.Stop(); 
+            //     anim.SetBool ("Walk", false);
+            //     WalkSFX.Stop();
             }
 
-            // Turning: Reverse if input is moving the Player right and Player faces left 
-            if ((hMove.x > 0 && !FaceRight) || (hMove.x < 0 && FaceRight)) {
+            // Turning. Reverse if input is moving the Player right and Player faces left.
+            if ((hvMove.x <0 && !FaceRight) || (hvMove.x >0 && FaceRight)){
                 playerTurn();
             }
         }
         clampPlayerMovement();
-    }
-
-    void FixedUpdate() {
-        //slow down on hills/stops sliding from velocity
-        if (hMove.x == 0) {
-            rb2D.velocity = new Vector2(rb2D.velocity.x / 1.1f, rb2D.velocity.y);
-        }
     }
 
     private void playerTurn(){
@@ -59,11 +52,12 @@ public class PlayerMove : MonoBehaviour
         transform.localScale = theScale;
     }
 
-    //keep player within camera bounds
-    //REMOVE IF NEEDED
+    //keep player within campera bounds
+    //REMOVE IF NOT NEEDED
     void clampPlayerMovement() {
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
