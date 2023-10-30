66f92a794895191683926da193d0644162fd1ed2
Saesha25 <147743989+Saesha25@users.noreply.github.com>
2023-10-30 12:10:40 -0400

    MainMenu
    Has the play, credits and instructions

---
+++ Banded_Together/Assets/Scripts/Play.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject creditsUI;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Eleanor_Workspace");
+
+    }
+
+    public void OpenCredits()
+    {
+        creditsUI.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsUI.SetActive(false);
+
+    }
+
+}
