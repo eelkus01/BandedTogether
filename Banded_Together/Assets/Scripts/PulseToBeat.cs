3f1cad7e934b210dd756d1f1a58425c4bedb0839
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2023-11-13 21:10:59 -0500

    fixed pulsing on beat

--- Banded_Together/Assets/Scripts/PulseToBeat.cs
+++ Banded_Together/Assets/Scripts/PulseToBeat.cs
@@ -8,17 +8,17 @@ public class PulseToBeat : MonoBehaviour {
     public float BPM = 100f;
     public Color StartColor;
     public Color EndColor;
-    private Material mat;
+    //private Color rendColor;
  
     // Use this for initialization
     void Start () {
-        mat = GetComponent<Renderer>().material;
+        //rendColor = GetComponent<SpriteRenderer>().color;
     }
     
     // Update is called once per frame
     void Update () {
         var baseValue = Mathf.Cos(((Time.time * Mathf.PI) * (BPM / 60f)) % Mathf.PI);
         var target = Color.Lerp(EndColor, StartColor, baseValue);
-        mat.SetColor("_EmissionColor", target);
+        GetComponent<Image>().color = target;
     }
 }
