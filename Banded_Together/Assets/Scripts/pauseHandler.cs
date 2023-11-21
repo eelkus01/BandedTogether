988360e1baff4d48e3b35fd00d82019474665011
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2023-11-21 16:49:37 -0500

    fixed quit button in pause

--- Banded_Together/Assets/Scripts/pauseHandler.cs
+++ Banded_Together/Assets/Scripts/pauseHandler.cs
@@ -5,56 +5,72 @@ using UnityEngine.UI;
 
 public class pauseHandler : MonoBehaviour
 {
-    bool GameIsPaused = false;
-    public GameObject menuUI;
     public Tween tw;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Resume();
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+    //public AudioMixer mixer;
+    //public static float volumeLevel = 1.0f;
+    //private Slider sliderVolumeCtrl;
+
+    void Awake (){
+        //NOTE: EVERYTHING BELOW IS VOR VOLUME SLIDER
+        //UNCOMMENT ONCE SLIDER IS IMPLEMENTED
+        // SetLevel(volumeLevel);
+        // GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+        // if (sliderTemp != null){
+        //     sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+        //     sliderVolumeCtrl.value = volumeLevel;
+        // }
+    }
+
+    void Start (){
+        pauseMenuUI.SetActive(false);
+        GameisPaused = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameisPaused){
                 Resume();
-            }
-            else
-            {
+            } else {
                 Pause();
             }
-
         }
-
-
     }
 
-    public void Pause()
+    void Pause()
     {
         Time.timeScale = 0f;
-        menuUI.SetActive(true);
+        pauseMenuUI.SetActive(true);
         tw.ButtonPop();
-        GameIsPaused = true;
-
+        GameisPaused = true;
     }
 
 
     public void Resume()
     {
         Time.timeScale = 1f;
-        menuUI.SetActive(false);
-        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+        GameisPaused = false;
     }
+
+    // public void SetLevel (float sliderValue){
+    //     mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+    //     volumeLevel = sliderValue;
+    // }
 
     public void Exit()
     {
+        // Application.Quit();
+        // Time.timeScale = 1f;
+        // pauseMenuUI.SetActive(false);
+        // GameisPaused = false;
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
-        Time.timeScale = 1f;
-        menuUI.SetActive(false);
-        GameIsPaused = false;
+        #endif
     }
 }
