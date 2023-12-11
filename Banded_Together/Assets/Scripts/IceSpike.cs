929f67e5bdb633ffa48899495a164f6d1a019049
miloshields <milo.shields@gmail.com>
2023-12-10 22:57:19 -0500

    functional ice attack (no freezing yet)

--- Banded_Together/Assets/Scripts/IceSpike.cs
+++ Banded_Together/Assets/Scripts/IceSpike.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class IceSpike : MonoBehaviour
 {
+    public float speed = 15.0f; // Speed of the ice spike
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Optional: Any initialization specific to the IceSpike
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 }
--- Banded_Together/Assets/Scripts/PlayerCast.cs
+++ Banded_Together/Assets/Scripts/PlayerCast.cs
@@ -27,39 +27,54 @@ public class PlayerCast : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // Check if the Space key is pressed and if enough time has passed since the last spawn.
         if (Input.GetKeyDown(KeyCode.Space)) {
             // check which instrument is selected, than calculate if that instrument is ready to fire again
             var selectedInstrument = gameHandler.GetComponent<GameHandler>().activeInstrumentID - 1;
+
+            Quaternion orientation;
             
             if(Time.time - lastSpawnTimes[selectedInstrument] >= cooldownDurations[selectedInstrument]) {
-                GameObject instance = Instantiate(attackPrefabs[selectedInstrument], transform.position, Quaternion.identity);
+                if(selectedInstrument == 2) {
+                    int direction = GetOrientation();
+                    int[] targetDirections = new int[] { direction - 15, direction, direction + 15 };
+                    foreach (int targetDirection in targetDirections)
+                    {
+                        Instantiate(attackPrefabs[selectedInstrument], transform.position, Quaternion.Euler(0f, 0f, targetDirection));
+                    }  
+                }
+                else {
+                    Instantiate(attackPrefabs[selectedInstrument], transform.position, Quaternion.identity);
+                }
                 lastSpawnTimes[selectedInstrument] = Time.time;
             }
         }
     }
 
     // for use in spawning ice spikes in the correct direction
-    public int GetPredominantDirection()
+    public static int GetOrientation()
     {
-        if (rb == null)
-        {
-            return -1; // Indicates an error or no movement
-        }
 
-        Vector2 velocity = rb.velocity;
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(velocity.x) > Mathf.Abs(velocity.y))
-        {
-            // Horizontal movement is predominant
-            return velocity.x > 0 ? 0 : 180; // 0 for right, 180 for left
-        }
-        else
-        {
-            // Vertical movement is predominant
-            return velocity.y > 0 ? 90 : 270; // 90 for up, 270 for down
-        }
+        // Calculate the angle in radians
+        float angle = Mathf.Atan2(y, x);
+
+        // Convert the angle to degrees
+        float degrees = angle * Mathf.Rad2Deg;
+
+        // Normalize the angle to be within 0 to 360 degrees
+        if (degrees < 0) degrees += 360;
+
+        // Round the angle to the nearest 45 degrees
+        int orientation = Mathf.RoundToInt(degrees / 45) * 45;
+
+        // Wrap around if the orientation is 360 degrees to make it 0 degrees
+        if (orientation == 360) orientation = 0;
+
+        return orientation;
     }
 }
