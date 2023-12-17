b88ae7ea45b642a9c45b076af8039835e3b0c419
jakefrie <jacobyfrie@gmail.com>
2023-12-17 13:32:29 -0500

    parts move

--- Banded_Together/Assets/Scripts/PartsPickup.cs
+++ Banded_Together/Assets/Scripts/PartsPickup.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class PartsPickup : MonoBehaviour {
     public GameHandler gameHandlerObj;
     public Image part;
+    //public Image bigPart;
+    public GameObject bigPartObject;
     //public playerVFX playerPowerupVFX;
 
     void Start(){
         gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
         part.gameObject.SetActive(false);
+        bigPartObject.gameObject.SetActive(false);
         //playerPowerupVFX = GameObject.FindWithTag("Player").GetComponent<playerVFX>();
     }
 
@@ -19,13 +22,29 @@ public class PartsPickup : MonoBehaviour {
             GetComponent<Collider2D>().enabled = false;
             //GetComponent<AudioSource>().Play();
             StartCoroutine(DestroyThis());
+            Destroy(gameObject.transform.GetChild(0).gameObject);
             gameHandlerObj.AddParts();
+            bigPartObject.gameObject.SetActive(true);
+
+            StartCoroutine(bigPartScale());
+
             part.gameObject.SetActive(true);
         }
     }
 
     IEnumerator DestroyThis(){
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(2.0f);
         Destroy(gameObject);
     }
+
+    IEnumerator bigPartScale()
+    {
+
+        LeanTween.scale(bigPartObject, new Vector3(.5f, .5f, .5f), 1f).setEase(LeanTweenType.easeOutBounce);
+        LeanTween.move(bigPartObject, bigPartObject.transform.position + new Vector3(200f, 100f, 0f), 1f);
+        yield return new WaitForSeconds(1);
+        bigPartObject.gameObject.SetActive(false);
+    }
 }
+
+
